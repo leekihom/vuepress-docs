70216a01bcef3ee386c0aff3f3cef85d93bf099e
leezihong <2578527296@qq.com>
2022-09-01 15:50:52 +0800

    更改了工具页的卡片布局样式

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,70 +1,60 @@
 <style>
-  .websiteList_bvLH{
-    display:grid;
-    gap:16px;
-    grid-template-columns:repeat(auto-fill,minmax(250px,1fr));
-    padding:0;
-  }
-  .websiteCard {
-  background-color: var(--ifm-card-background-color);
-  border-radius: 8px;
-  border: 1px solid rgba(204, 204, 204, 0.5);
+
+.card-list{
+  margin: -.35rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill,minmax(250px,1fr));
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+.card-list a{
+  height: 88px;
+}
+.card-item{
+  margin: .35rem;
+  border-radius:  3px;
   display: flex;
-  flex-direction: row;
+  transition: all .4s;
+}
+.card-item:hover{
+  transform: translateY(-3px) scale(1.01);
+  text-decoration: none !important;
+}
+.name{
+  margin: .2rem 0 .3rem;
+}
+.desc{
+  font-size: .8rem;
+  line-height: 1.1rem;
+  opacity: .8;
+  margin-bottom: .2rem;
   overflow: hidden;
-  transition: all 0.3s ease-in-out;
-}
-
-.websiteCard:hover {
-  box-shadow: 5px 5px 20px 0 rgba(0, 0, 0, 0.1);
-  background-color: #f2f8fd;
-  transform: translateY(-5px);
-}
-
-.websiteCardImage {
-  align-self: center;
-  width: 64px;
-  height: 64px;
-  min-width: 64px;
-}
-
-.websiteCardHeader {
-  display: flex;
-  align-items: center;
-  margin-bottom: 4px;
-}
-
-.websiteCardTitle {
-  margin-bottom: 0;
-  flex: 1 1 auto;
-}
-
-.websiteCardTitle a {
-  text-decoration: none;
-  background: linear-gradient(
-      var(--ifm-color-primary),
-      var(--ifm-color-primary)
-    )
-    0% 100% / 0% 1px no-repeat;
-  transition: background-size ease-out 200ms;
-}
-
-.websiteCardTitle a:not(:focus):hover {
-  background-size: 100% 1px;
-}
-
-.websiteCardBody {
-  padding: 0.2rem 0 0 var(--ifm-card-horizontal-spacing);
-}
-
-.websiteCardDesc {
-  cursor: pointer;
-  font-size: smaller;
-  line-height: 1.66;
-  width: 100%;
-  margin: 0;
   display: -webkit-box;
-  overflow: hidden;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+.card-list p{
+  margin: 0;
+  padding: 0 1rem;
+  transition: text-shadow .4s;
+  text-align: center;
+}
+.card-list div{
+  flex: 1;
+  display: inline-block;
+  padding: 1rem 0;
+}
+.card-img{
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  margin: 1rem 0 1rem 1rem;
+  box-shadow: 3px 2px 5px rgb(0 0 0 / 8%);
+  transition: all .4s;
+}
+.row-3{
+  width: calc(33.33333% - .7rem);
 }
 
 </style>
@@ -78,28 +68,32 @@ export default {
           desc: '程序员一站式编程导航，专注于程序员学习编程提效，官网',
           logo: 'https://www.coderutil.com/favicon.ico',
           href: 'https://www.coderutil.com/',
-          tags: [''],
+          bgColor: 'linear-gradient(0deg,#a18cd1 0%, #fbc2eb 100%)',
+          color: ' rgb(104, 84, 161)',
         },
         {
         name: 'Road To Coding',
         desc: 'Road To Coding，意为「编程自学之路」，是自学编程以来所用资源和分享内容的大聚合',
         logo: 'https://r2coding.com/favicon.ico',
         href: 'https://r2coding.com/',
-        tags: [''],
+        bgColor: 'rgb(203, 234, 250)',
+        color: ' rgb(104, 84, 161)',
       },
       {
         name: '印记中文',
         desc: '深入挖掘国外前端新领域，为中国 Web 前端开发人员提供优质文档！',
         logo: 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/al1nd-ai8jt.png',
         href: 'https://docschina.org/',
-        tags: ['前端', '文档'],
+        bgColor: 'rgb(203, 234, 250)',
+        color: ' rgb(104, 84, 161)',
       },
       {
         name: 'ES6 入门教程',
         desc: '《ECMAScript 6 入门教程》是一本开源的 JavaScript 语言教程，全面介绍 ECMAScript 6 新引入的语法特性。',
-        logo: '/img/website/es6.png',
+        logo: 'https://r2coding.com/favicon.ico',
         href: 'https://es6.ruanyifeng.com/',
-        tags: ['前端', '文档'],
+        bgColor: 'rgb(203, 234, 250)',
+        color: ' rgb(104, 84, 161)',
       },
       ]
     }
@@ -109,26 +103,19 @@ export default {
 
 <template>
 
-  <ul class="websiteList_bvLH">
-    <div v-for="item in list" :key="item.name">
-    <li
-    class="websiteCard">
-    <img
-      :src="item.logo"
-      :alt="item.name"
-      class="websiteCardImage"
-    />
-    <div class="websiteCardBody">
-      <div class="websiteCardHeader">
-        <h4 class="websiteCardTitle">
-          <a href={{item.href}} class="websiteCardLink">
-            {{item.name}}
-          </a>
-        </h4>
+  <div class="card-list">
+
+    <a v-for="item in list" :key="item.name"
+    class="card-item row-3" :href="item.href" target="_blank" :style="{'background-image': item.bgColor,color: item.color}">
+      <img
+        :src="item.logo"
+        :alt="item.name"
+        class="card-img"
+      />
+      <div>
+        <p class="name">{{item.name}}</p>
+        <p class="desc">{{item.desc}}</p>
       </div>
-      <p class="websiteCardDesc">{{item.desc}}</p>
-    </div>
-  </li>
+    </a>
   </div>
-  </ul>
 </template>
